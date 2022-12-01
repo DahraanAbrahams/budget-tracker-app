ff312e710f8601c37a54d68e069a67bf2d8790ac
Dahraan Abrahams <dahraanabrahams@Dahraans-MacBook-Air.local>
2022-12-01 13:23:38 +0200

    Update backend server

--- server/server.js
+++ server/server.js
@@ -21,6 +21,12 @@ app.use('/api/transactions', require('./routes/transactionRoutes'))
 app.use('/api/budget', require('./routes/budgetRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+const proxy = require('http-proxy-middleware')
+
+module.exports = function(app) {
+    app.use(proxy(['/api' ], { target: 'http://localhost:5001' }));
+} 
+
 //server frontend
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
@@ -32,8 +38,8 @@ if (process.env.NODE_ENV === 'production') {
     )
   } else {
     app.get('/', (req, res) => res.send('Please set to production'));
-  }
-
+}
+  
 dbConnect.then(db => { 
     if (!db) return process.exit(1)
     

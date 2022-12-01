52ced765e4abbbec21a71be5dda45f1d890c499e
Dahraan Abrahams <dahraanabrahams@Dahraans-MacBook-Air.local>
2022-12-01 18:00:48 +0200

    Update backend to serve frontend

--- server/server.js
+++ server/server.js
@@ -23,11 +23,11 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 //server frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
   
     app.get('*', (req, res) =>
       res.sendFile(
-        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+        path.resolve(__dirname, '..', 'client', 'build', 'index.html')
       )
     )
   } else {

f9798f4fd1e8d71f6134bf5d7c6853984602cbb1
Dahraan Abrahams <dahraanabrahams@Dahraans-MacBook-Air.local>
2022-12-01 12:32:44 +0200

    Update backend server

--- server/server.js
+++ server/server.js
@@ -23,15 +23,16 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 //server frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  
     app.get('*', (req, res) =>
-        res.sendFile(
-            path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-        ))
-} else { 
-    app.get('/', (req, res) => res.send('Please set to production!'))
-}
+      res.sendFile(
+        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      )
+    )
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 dbConnect.then(db => { 
     if (!db) return process.exit(1)

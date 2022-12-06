5ea433f519b0e1031694dcd2d5cbe23a82ec6c3f
Dahraan Abrahams <dahraanabrahams@Dahraans-MacBook-Air.local>
2022-12-06 15:45:47 +0200

    Update baseApi URI

--- client/src/features/api/baseApi.js
+++ client/src/features/api/baseApi.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseURI = '/'
-// const baseURI = 'http://localhost:5001'
+const baseURI = 'http://localhost:5001'
 
 export const baseApi = createApi({
     baseQuery: fetchBaseQuery({
